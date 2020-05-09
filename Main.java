fef2a1b870e98a4838a7882302677f93a0a67078
iashmlv <63490977+iashmlv@users.noreply.github.com>
2020-05-09 17:26:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         double regularBonus = 0.3;
         double specialBonus = 0.6;
         double totalBonus = regularBonus + specialBonus;
